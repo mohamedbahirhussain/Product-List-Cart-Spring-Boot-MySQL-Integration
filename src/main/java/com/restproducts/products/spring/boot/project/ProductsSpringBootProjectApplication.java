28157a12605c85c8d0bba8082dfb07b7057e5cf5
Bahir Hussain <mbahirh624@gmail.com>
2025-03-04 16:43:53 +0300

    Test Spring

---
+++ src/main/java/com/restproducts/products/spring/boot/project/ProductsSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.restproducts.products.spring.boot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restproducts/products/spring/boot/project/ProductsSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restproducts.products.spring.boot.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
