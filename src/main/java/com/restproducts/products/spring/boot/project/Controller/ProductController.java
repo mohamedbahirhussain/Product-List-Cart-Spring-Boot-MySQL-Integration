c86fb35a57ba821aa8596c1280af30eade24feeb
Bahir Hussain <mbahirh624@gmail.com>
2025-10-03 14:25:37 +0300

    Updated

--- src/main/java/com/restproducts/products/spring/boot/project/Controller/ProductController.java
+++ src/main/java/com/restproducts/products/spring/boot/project/Controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.restproducts.products.spring.boot.project.Controller;
 
 import com.restproducts.products.spring.boot.project.Model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
 
     public String addProduct(@RequestBody Product product){
 
---
+++ src/main/java/com/restproducts/products/spring/boot/project/Repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.restproducts.products.spring.boot.project.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository {
+}
